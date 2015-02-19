dd8f88f45d0afedd830faa9483f0cc576fb4c8e7
mixalot2009 <mixalot2009@gmail.com>
2015-02-19 16:58:32 -0500

    commit 12

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //Christofer Mix 02/19/2015 Conditionals
 var PokemonHealth = 300;
-var PokemonStatus
+var PokemonStatus;
 PokemonStatus = (PokemonHealth) ? "Your opponents health is full." : "Your opponents pokemon is weakened.";
 console.log(PokemonStatus);
 alert("The opponents pokemon has " + PokemonHealth + "HP left.");
-alert(PokemonStatus)
+alert(PokemonStatus);
 var PokemonAttackType = confirm("Is the type of attack a fire type attack?");
 //If the attack is a fire type it will be doubled, if not the it will be halved.
 var PokemonAttackPP = prompt("How many PP does your pokemon have left?");
@@ -15,21 +15,21 @@ console.log("The opponents pokemon has " + PokemonHealth + "HP left.");
 console.log("Your Pokemon has " + PokemonAttackPP + "PP left.");
 if(PokemonAttackType){
     PokemonAttackBonus = PokemonAttackPower * 2;
-    console.log("Your attack was super effective!")
-    alert("Your attack was super effective!")
+    console.log("Your attack was super effective!");
+    alert("Your attack was super effective!");
 }else{
     PokemonAttackBonus = PokemonAttackPower / 2;
-    console.log("Your Attack was not very effective!")
-    alert("Your Attack was not very effective!")
+    console.log("Your Attack was not very effective!");
+    alert("Your Attack was not very effective!");
 }
 //The attack type determines if the attack does double or half the original damage would
-alert("Your Pokemon\'s attack did " + PokemonAttackBonus + "damage!")
+alert("Your Pokemon\'s attack did " + PokemonAttackBonus + "damage!");
 console.log("Your Pokemon did " + PokemonAttackBonus + "damage!");
-if(PokemonAttackPP > 1 && PokemonAttackBonus >= 300){
-    console.log("Your opponents pokemon fainted!")
+if(PokemonAttackPP > 0 && PokemonAttackBonus >= 300){
+    console.log("Your opponents pokemon fainted!");
     alert("Your opponents pokemon fainted!")
 }else{
-    console.log("You did not knock out the opponent\'s pokemon!")
+    console.log("You did not knock out the opponent\'s pokemon!");
     alert("You did not knock out the opponent\'s pokemon!")
 }
 //If the doubled or halved damage equals or exceeds 300 damage the pokemon is knocked out.
