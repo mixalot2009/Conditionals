724fd07b6283bf239f593d7916b826567e0591e5
mixalot2009 <mixalot2009@gmail.com>
2015-02-19 14:55:25 -0500

    commit 5

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 //Christofer Mix 02/19/2015 Conditionals
 var PokemonHealth = 300;
+alert("The opponents pokemon has " + PokemonHealth + "HP left.");
 var PokemonAttackType = confirm("Is the type of attack a fire type attack?");
 var PokemonAttackPP = prompt("How many PP does your pokemon have left?");
 var PokemonAttackPower = prompt("What is the Pokemon's attack power?");
 console.log("The opponents pokemon has " + PokemonHealth + "HP left.");
 console.log("Your Pokemon has " + PokemonAttackPP + "PP left.");
 if(PokemonAttackType){
-    PokemonAttackBonus = PokemonAttackPower * 2
+    PokemonAttackBonus = PokemonAttackPower * 2;
     console.log("Your attack was super effective!")
+    alert("Your attack was super effective!")
 }else{
-    PokemonAttackBonus = PokemonAttackPower / 2
+    PokemonAttackBonus = PokemonAttackPower / 2;
     console.log("Your Attack was not very effective!")
-};
-console.log("Your Pokemon did " + PokemonAttackBonus + "Damage!")
+    alert("Your Attack was not very effective!")
+}
+console.log("Your Pokemon did " + PokemonAttackBonus + " Damage!");
 if(PokemonAttackPP > 1 && PokemonAttackBonus >= 300){
     console.log("Your opponents pokemon fainted!")
 }else{
     console.log("You did not knock out the opponents pokemon!")
-};
+}
